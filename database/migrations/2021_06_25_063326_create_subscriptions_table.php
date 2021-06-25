66bc2f675dd065dc9a4f43a482349057e5fc70b3
unknown <eden.abdisa@yahoo.com>
2021-06-25 10:09:29 +0300

    Subscription model and migration added

---
+++ database/migrations/2021_06_25_063326_create_subscriptions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned()->primary(); 
+            $table->bigInteger('type_id',10); 
+            $table->bigInteger('user_id')->unsigned();
+            $table->timestamps();            
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('type_id')->references('id')->on('types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+}
