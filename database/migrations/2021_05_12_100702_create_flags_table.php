414a40b87f8fb85f19d2a4a5b116b294cf7055ea
edenAbdisa <edenabdisa90@gmail.com>
2021-06-30 17:42:23 +0300

    Reference key edited on report types for flag

--- database/migrations/2021_05_12_100702_create_flags_table.php
+++ database/migrations/2021_05_12_100702_create_flags_table.php
@@ -15,12 +15,12 @@ class CreateFlagsTable extends Migration
     {
         Schema::create('flags', function (Blueprint $table) {
             $table->bigIncrements('id');
-		    $table->string('reason',50);
+		    $table->bigInteger('reason_id');
 		    $table->bigInteger('flagged_item_id')->unsigned();
             $table->bigInteger('flagged_by_id')->unsigned();
 		    $table->string('type',30)->default('item'); 
             $table->timestamps();
-            $table->foreign('reason')->references('report_detail')->on('report_types');
+            $table->foreign('reason_id')->references('id')->on('report_types');
             $table->foreign('flagged_by_id')->references('id')->on('users');
         });
     }
