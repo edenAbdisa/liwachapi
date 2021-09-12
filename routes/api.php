980747c47fd02379bd52f113da421a67d2fa8967
edenAbdisa <eden.abdisa@yahoo.com>
2021-09-12 09:28:34 +0300

    scope added

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Http\Controllers;
 |
 */
 
-/* Route::middleware('auth:api')->get('/user', function (Request $request) {
+/* Route::middleware(['auth:api','scope:user;admin;organization'])->get('/user', function (Request $request) {
 return $request->user();
 }); */
 //Route::middleware('auth:api')->group(function (){
