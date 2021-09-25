fc64d6495470855637ac284df9c71085d5cb51e7
edenAbdisa <eden.abdisa@yahoo.com>
2021-09-25 19:20:35 +0300

    user transaction

---
+++ database/migrations/2021_09_25_161326_create_user_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned()->nullable();
+            $table->date('refreshed_on');
+            $table->integer('limit_of_post')->nullable();
+            $table->integer('transaction_limit')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_transactions');
+    }
+}
